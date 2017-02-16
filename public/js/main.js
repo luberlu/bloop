bcb942c699f0588e55f13843782399a77da2a0c1
Lucien Hubert <phillipehubert@gmail.com>
2017-02-16 20:00:26 +0100

    no current time error

--- public/js/main.js
+++ public/js/main.js
@@ -144,8 +144,11 @@ $(function() {
     // Play sound html5
 
     var playSound = function (link) {
-        sounds.audioObjects[link.toLowerCase()].currentTime = 0;
-        sounds.audioObjects[link.toLowerCase()].play();
+
+        if(typeof sounds.audioObjects[link.toLowerCase()].currentTime !== "undefined") {
+            sounds.audioObjects[link.toLowerCase()].currentTime = 0;
+            sounds.audioObjects[link.toLowerCase()].play();
+        }
     };
 
     // DOM
